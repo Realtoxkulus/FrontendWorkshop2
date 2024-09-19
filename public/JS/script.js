d465d72629ad6e54972ce645f11c6dc7e3c0ad16
Jui <pathanin.pra@dome.tu.ac.th>
2024-09-19 09:59:50 +0700

    Add Call rest API in JS

--- public/JS/script.js
+++ public/JS/script.js
@@ -41,3 +41,19 @@ allInputs.forEach(input => {
         }
     })
 })
+
+function call_REST_API_Hello() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const url = (
+        'http://localhost:8080/hello?' +
+        new URLSearchParams({ myName: username, lastName: password}).toString()
+      );
+    
+    fetch(url)
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => console.error('Error:', error));
+}
